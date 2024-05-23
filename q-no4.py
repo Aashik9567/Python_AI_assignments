12fd3e960c84b79be335001364ebaf3f0b020934
Aashik9567 <aashikmahato9567@gmail.com>
2024-05-23 21:40:29 +0545

    Minor bug fixed

--- q-no4.py
+++ q-no4.py
@@ -6,5 +6,5 @@ def fibonacci_generator(n):
 
 
 num_term = 10
-print(f"The first {num_term} terms of the Fibonacci series are:")
+print(f"The first {num_term} of the Fibonacci series are:")
 print(list(fibonacci_generator(num_term)))
